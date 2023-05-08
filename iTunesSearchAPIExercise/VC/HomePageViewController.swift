f613814bca6cb1990966b4719877f72f679522f1
DanielChen <>
2023-05-08 16:17:09 +0800

    Implementation UI elements delegate

--- iTunesSearchAPIExercise/VC/HomePageViewController.swift
+++ iTunesSearchAPIExercise/VC/HomePageViewController.swift
@@ -13,9 +13,41 @@ class HomePageViewController: UIViewController {
     @IBOutlet weak var musicInfoView: MusicInformationView!
     @IBOutlet weak var searchView: SearchView!
     
+    // MARK: - override
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        musicListTableView.dataSource = self
+        musicListTableView.delegate = self
+        playerView.delegate = self
+        searchView.delegate = self
     }
 }
 
+// MARK: - UITableViewDataSource
+extension HomePageViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell.init()
+    }
+    
+}
+// MARK: - UITableViewDelegate
+extension HomePageViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+}
+// MARK: - SearchViewDelegate
+extension HomePageViewController: PlayerViewDelegate {
+    func clickPlayButton(state: PlayerState) {
+        
+    }
+}
+// MARK: - SearchViewDelegate
+extension HomePageViewController: SearchViewDelegate {
+    func clickSearchButton(searchText: String?) {
+    }
+}
