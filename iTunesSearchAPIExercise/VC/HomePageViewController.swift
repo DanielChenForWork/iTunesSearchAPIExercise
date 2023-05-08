3d4a65dd773c1e339ee444f6c79d5692cbbd50a2
DanielChen <>
2023-05-08 16:14:00 +0800

    Add UI elements to main storyboard

--- iTunesSearchAPIExercise/VC/HomePageViewController.swift
+++ iTunesSearchAPIExercise/VC/HomePageViewController.swift
@@ -8,12 +8,14 @@
 import UIKit
 
 class HomePageViewController: UIViewController {
-
+    @IBOutlet weak var musicListTableView: UITableView!
+    @IBOutlet weak var playerView: PlayerView!
+    @IBOutlet weak var musicInfoView: MusicInformationView!
+    @IBOutlet weak var searchView: SearchView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
