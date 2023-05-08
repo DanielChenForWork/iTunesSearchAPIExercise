53ce8d0a7cd52c2c553d432dd47ff4426e61a227
DanielChen <>
2023-05-08 16:56:14 +0800

    fixed cell height

--- iTunesSearchAPIExercise/VC/HomePageViewController.swift
+++ iTunesSearchAPIExercise/VC/HomePageViewController.swift
@@ -59,6 +59,10 @@ extension HomePageViewController: UITableViewDataSource {
 }
 // MARK: - UITableViewDelegate
 extension HomePageViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     }
 }
